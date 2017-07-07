663e6c179d97b38fb2da48554af6e64ea3f4929f
Development Intern <developmentintern@CastNets-MacBook-Air.local>
2017-07-07 11:33:24 -0700

    First commit with firebase; basic adding and deleting todos works

--- js/index.js
+++ js/index.js
@@ -1,60 +1,81 @@
 angular.module('todoApp', [])
-  .controller('TodoListController', function() {
+  .controller('TodoListController', function($scope) {
     // initialize variables
     var todoList = this;
-    var idCounter = 100;
+    //var idCounter = 100;
 
     todoList.todoText = "";
     todoList.todos = [];
 
+    firebase.database().ref().on('value', function(snapshot) {
+      var i = 0;
+      todoList.todos = [];
+      snapshot.forEach(function(todo) {
+        todoList.todos[i] = todo.val();
+        todoList.todos[i].key = todo.key;
+        i++;
+      })
+      if(!$scope.$$phase) {
+        $scope.$apply();
+      }
+    });
+
     // load data from localStorage if data is there
-    if (localStorage.getItem('todoList') != null) {
+    /*if (localStorage.getItem('todoList') != null) {
       todoList.todos = JSON.parse(localStorage.getItem('todoList') );
       idCounter = JSON.parse(localStorage.getItem('todoListIdCounter'));
-    }
+    }*/
 
     todoList.addTodo = function() {
       if (todoList.todoText == "") {
         todoList.todoText = "empty task";
       }
 
-      todoList.todos.push({
+      firebase.database().ref().push({
+        text: todoList.todoText,
+        done: false
+      });
+
+      /*todoList.todos.push({
         id:idCounter,
         timeStamp: new Date(),
         text:todoList.todoText,
         done:false,
         editing:false
-      });
-      idCounter++;
-      todoList.updateLocalStorage();
+      });*/
+      //idCounter++;
+      //todoList.updateLocalStorage();
 
       todoList.todoText = "";
       document.getElementById("addTodoElement").focus();
     }
 
     todoList.editTodo = function(index) {
-      todoList.todos[index].editing=true;
+      /*todoList.todos[index].editing=true;
       elementId = "editTodoElement" + index;
-      document.getElementById(elementId).select();
+      document.getElementById(elementId).select();*/
     }
 
     todoList.doneEditing = function(index) {
-      todoList.todos[index].editing=false;
+      /*todoList.todos[index].editing=false;
       if (todoList.todos[index].text == "") {
         todoList.todos[index].text = "empty task";
       }
 
-      todoList.updateLocalStorage();
+      todoList.updateLocalStorage();*/
     }
 
     todoList.deleteTodo = function(index) {
-      todoList.todos.splice(index, 1);
-      todoList.updateLocalStorage();
+      firebase.database().ref('/' + todoList.todos[index].key).remove();
+      //todoList.todos.splice(index, 1);
+      //todoList.updateLocalStorage();*/
     }
 
     todoList.updateLocalStorage = function() {
-      localStorage.setItem('todoList', JSON.stringify(todoList.todos));
-      localStorage.setItem('todoListIdCounter', JSON.stringify(idCounter));
+      /*firebase.database().ref().push({
+        text: "hello",
+        done: false
+      });*/
     }
 
   });
